75c2ce8383b890080c0e48db7964f43649696499
Korou <koroudakshana@gmail.com>
2021-05-25 22:26:41 +0530

    using port from env if available

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express=require('express')
 
 const app=express()
 
+const SERVER_PORT=process.env.PORT || 4334
 
 app.use('/public',express.static(__dirname +'/public'))
 
@@ -11,6 +12,6 @@ app.get('/',(req,res)=>{
     res.send('Currently this is from backend')
 })
 
-app.listen(4334,()=>{
+app.listen(SERVER_PORT,()=>{
     console.log('Server started on http://localhost:4334')
 })
